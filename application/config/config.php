50ceee6e17b5a4e8726d289bba37f15f09ab7fb2
apple <apple@apples-MacBook-Air.local>
2023-01-29 14:24:39 +0700

    print com guide

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost:8888/cms_sribhurapa';
-$config['base_url'] = 'https://merchant-cms.sborchid9.com'; //Product
+$config['base_url'] = 'http://localhost:8888/cms_sribhurapa';
+//$config['base_url'] = 'https://merchant-cms.sborchid9.com'; //Product
 //$config['base_url'] = 'https://st-merchant-cms.sborchid9.com'; //Staging
 
 
